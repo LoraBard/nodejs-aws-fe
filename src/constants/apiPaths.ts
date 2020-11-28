cc5f04bd54b6bf20802e280fc491de2c64bd533a
thetsman <Tatsiana_Hetsman@epam.com>
2020-11-28 15:20:09 +0300

    feat: add task 6

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 
 const API_PATHS = {
-  product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  product: 'https://rhhk83iicl.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  bff: 'https://l0bupdv27h.execute-api.eu-west-1.amazonaws.com/dev'
+  import: 'https://6du3gvv6cd.execute-api.eu-west-1.amazonaws.com/dev',
+  bff: 'https://rhhk83iicl.execute-api.eu-west-1.amazonaws.com/dev'
 };
 
 export default API_PATHS;
